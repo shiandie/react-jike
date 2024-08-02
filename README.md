6774cd316d5d066ace57f2f488fea39624d4c325
shiandie <shiandie86@gmail.com>
2024-08-02 09:12:50 +0800

    校验和收集表单数据

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 功能实现
+
+## 登录功能
+
+### 校验登录表单
+
+1.  给 form 设置 validateTrigger 属性，校验触发事件
+2.  **给 form.item 设置 name 属性**
+3.  给 form.item 设置 rules 属性，设置校验规则
+
+### 获取登录表单数据
+
+1. 给 form 设置 onfinish 属性 获取表单数据
+
+```js
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+```
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,49 @@
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+import logo from "@/assets/logo.png";
+
 const Login = () => {
-  return <div>Login</div>;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form
+          onFinish={onFinish}
+          initialValues={{ mobile: "13811111111", code: "246810" }}
+          validateTrigger={["onBlur"]}
+        >
+          <Form.Item
+            name="mobile"
+            rules={[
+              { required: true, message: "请输入手机号" },
+              {
+                required: true,
+                pattern: /^1[3-9]\d{9}$/,
+                message: "手机号格式不正确",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[{ required: true, message: "请输入验证码" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
+
 export default Login;
